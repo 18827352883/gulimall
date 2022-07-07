3947815a8a0a3753f241ead3153a065433784a10
root <Hbw2466512236@163.com>
2022-07-07 23:26:25 +0800

    项目初始结构

---
+++ gulimall-member/src/test/java/com/huayu/gulimallmember/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huayu.gulimallmember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
